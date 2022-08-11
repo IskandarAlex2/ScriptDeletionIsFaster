5ee4a61ac00151847f3e16c39efefd7eeef111df
IskandarAlex2 <65650645+IskandarAlex2@users.noreply.github.com>
2022-08-11 20:52:22 +0800

    First

---
+++ Deletor.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+const Dir = fs.readdirSync("junks");
+
+var Sizes = 0;
+var HowMany = 0;
+
+function TimeStamp() {
+    const utc = new Date().getTime();
+    return utc;
+}
+
+const StartTime = TimeStamp();
+ 
+Dir.forEach((val) => {
+    if (fs.existsSync(`junks/${val}`)) {
+        Sizes+=(fs.statSync(`junks/${val}`).size);
+        fs.unlinkSync(`junks/${val}`);
+        HowMany++;
+    }
+})
+
+console.log(`Deleted ${HowMany} files (Size total of ${Sizes / 1000000} MB) in ${(new Date().getTime() - StartTime) / 1000} Seconds!`);
